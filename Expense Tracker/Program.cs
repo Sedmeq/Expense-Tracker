490d9935a50d25ca4b5210ed15e1f2c68e5e4c30
Sedmeq <Sedmeq@users.noreply.github.com>
2025-08-05 15:04:37 +0400

    Add project files.

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,32 @@
+using Expense_Tracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCd0x1WmFZfVtgfV9GZlZVRGYuP1ZhSXxWdk1jXX9XcXJVQWZUVUR9XEI=");
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
